c5607cc9453a9b44498aeda5670662b38c731889
PierreZboril-Berteaud <pierre.zboril-berteaud@isen-ouest.yncrea.fr>
2025-06-23 11:48:34 +0200

    a

--- js/ajaxrequest.js
+++ js/ajaxrequest.js
@@ -1,40 +1,38 @@
 function ajaxRequest(type, url, callback, data = null)
 {
-  /* global XMLHttpRequest 
-  @param type: string (GET, POST, PUT, DELETE)
-  @param url: string (url of the request) 
-  @param callback: function (function to call when the request is successful)
-  @param data: string (data to send with the request)
-  @return void
-  */
-  
-  let xhr;
+  let xhr = new XMLHttpRequest();
 
-  // Create XML HTTP request.
-  xhr = new XMLHttpRequest();
-  if (type == 'GET' && data != null)
+  if (type === 'GET' && data != null)
     url += '?' + data;
+
   xhr.open(type, url);
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-  // Add the onload function.
   xhr.onload = () =>
   {
     switch (xhr.status)
     {
       case 200:
       case 201:
-        console.log(xhr.responseText);
-        callback(JSON.parse(xhr.responseText));
+        if (xhr.responseText.trim() !== "") {
+          try {
+            callback(JSON.parse(xhr.responseText));
+          } catch (e) {
+            console.warn("Réponse non JSON, appel direct du callback.");
+            callback(); // on appelle quand même
+          }
+        } else {
+          callback(); // si pas de réponse, on appelle juste la fonction
+        }
         break;
       default:
         httpErrors(xhr.status);
     }
   };
 
-  // Send XML HTTP request.
   xhr.send(data);
 }
+
 
 
 
