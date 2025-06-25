0c8711ecc87f93e94d01aed7f614f611a5ebb789
ThomasRou35 <th.rouleau44@gmail.com>
2025-06-25 12:07:27 +0200

    send

--- js/affichageNavire.js
+++ js/affichageNavire.js
@@ -3,126 +3,213 @@ $("#AffichageNavire").click(() => {
   ajaxRequest('GET', '../php/request.php?action=AfficheTableau', DisplayTablePage);
 });
 
-
 //fonction appelée après la réception des données des bateaux 
-function DisplayTablePage(){
+function DisplayTablePage() {
     clearPage(); //vider le contenu principal
-    ajaxRequest('GET','../php/request.php?action=GetNavire',displayNavireTable)
+    ajaxRequest('GET', '../php/request.php?action=GetNavire', displayNavireTable);
 }
 
-
 function displayNavireTable(response) {
-  let html = `
-  <h3> Liste des bateaux </h3>
-  <div class="scroller">
-      <table id="tabletable_bateau" class="container">
-          <thead>
-              <tr>
-                  <th>MMSI</th>
-                  <th>Hordatage</th>
-                  <th>Latitude</th>
-                  <th>Longitude</th>
-                  <th>SOG</th>
-                  <th>COG</th>
-                  <th>Cap réel</th>
-                  <th>Nom</th>
-                  <th>Etat</th>
-                  <th>Longueur</th>
-                  <th>Largeur</th>
-                  <th>Tirant d'eau</th>
-                  <th>Prediction</th>
-              </tr>
-          </thead>
-        </table>
+    let html = `
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Liste des bateaux</h3>
+            <div class="scroller">
+                <table id="tabletable_bateau" class="container">
+                    <thead>
+                        <tr>
+                            <th>MMSI</th>
+                            <th>Hordatage</th>
+                            <th>Latitude</th>
+                            <th>Longitude</th>
+                            <th>SOG</th>
+                            <th>COG</th>
+                            <th>Cap réel</th>
+                            <th>Nom</th>
+                            <th>Etat</th>
+                            <th>Longueur</th>
+                            <th>Largeur</th>
+                            <th>Tirant d'eau</th>
+                            <th>Prediction</th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+            </div>
+            
+            <div class="d-flex justify-content-between my-3">
+                <button id="predictButton" class="btn btn-primary">Prédire Le Type</button>
+                <button id="positionButton" class="btn btn-primary">Prédire La Position</button>
+                <button id="plotMapButton" class="btn btn-success">Afficher sur la carte</button>
+            </div>
+        </div>
+        
+        <div class="col-md-6">
+            <h3>Carte des bateaux</h3>
+            <div id="mapDiv" style="height: 600px; border: 1px solid #ddd; border-radius: 4px;"></div>
+        </div>
     </div>
-          `; 
+    `; 
+    
     $('#TableauNavire').html(html);
 
-  if (response.length !== 0) {
-  for (let i = 0; i < response.length; i++) {
-    let row = `
-      <tr data-index="${i}">
-        <td>${response[i].mmsi}</td>
-        <td>${response[i].basedatetime}</td>
-        <td>${response[i].lat}</td>
-        <td>${response[i].lon}</td>
-        <td>${response[i].sog}</td>
-        <td>${response[i].cog}</td>
-        <td>${response[i].heading}</td>
-        <td>${response[i].nom_bateau}</td>
-        <td>${response[i].vesselstatus}</td>
-        <td>${response[i].longueur}</td>
-        <td>${response[i].largeur}</td>
-        <td>${response[i].draft}</td>
-        <td>
-          <input type="radio" name="Prediction-Type-Navire" class="predict-radio" data-index="${i}">
-        </td>
-    </tr>
-    `;
-    $("table.container").append(row);
+    if (response.length !== 0) {
+        let tbody = $('#tabletable_bateau tbody');
+        for (let i = 0; i < response.length; i++) {
+            let row = `
+            <tr data-index="${i}">
+                <td>${response[i].mmsi}</td>
+                <td>${response[i].basedatetime}</td>
+                <td>${response[i].lat}</td>
+                <td>${response[i].lon}</td>
+                <td>${response[i].sog}</td>
+                <td>${response[i].cog}</td>
+                <td>${response[i].heading}</td>
+                <td>${response[i].nom_bateau}</td>
+                <td>${response[i].vesselstatus}</td>
+                <td>${response[i].longueur}</td>
+                <td>${response[i].largeur}</td>
+                <td>${response[i].draft}</td>
+                <td>
+                    <input type="radio" name="Prediction-Type-Navire" class="predict-radio" data-index="${i}">
+                </td>
+            </tr>
+            `;
+            tbody.append(row);
+        }
+        
+        // Afficher tous les bateaux sur la carte par défaut
+        plotAllBoatsOnMap(response);
+    }
     
-    }
-  }
-  
-  let button=`
-  <div class="d-flex justify-content-between my-3">
-    <button id="predictButton" class="btn btn-primary">Prédire Le Type</button>
-    <button id="positionButton" class="btn btn-primary">Prédire La Position</button>
-  </div>`;
+    // Gestion des événements
+    $('#plotMapButton').click(() => {
+        let selectedIndex = $("input[name='Prediction-Type-Navire']:checked").data('index');
+        if (selectedIndex !== undefined) {
+            plotSelectedBoatOnMap(response, selectedIndex);
+        } else {
+            plotAllBoatsOnMap(response);
+        }
+    });
 
-  $('#TableauNavire').append(button);
+    $('#predictButton').click(() => {
+        let selectedIndex = $("input[name='Prediction-Type-Navire']:checked").data('index');
+        if (selectedIndex !== undefined) {
+            const data = {
+                mmsi: response[selectedIndex].mmsi,
+                Length: response[selectedIndex].longueur,
+                Width: response[selectedIndex].largeur,
+                Draft: response[selectedIndex].draft
+            };
+            const queryString = new URLSearchParams(data).toString();
+            ajaxRequest('POST', '../php/request.php?action=predictType', displayPredictPage, queryString);
+        } else {
+            alert("Veuillez sélectionner un navire pour la prédiction.");
+        }
+    });
 
-  $('#predictButton').click(() => {
-    let selectedIndex = $("input[name='Prediction-Type-Navire']:checked").data('index');
+    $('#positionButton').click(() => {
+        let selectedIndex = $("input[name='Prediction-Type-Navire']:checked").data('index');
+        if (selectedIndex !== undefined) {
+            let timeInput = prompt("Entrez le temps (en secondes) pour la prédiction de position :");
+            let time = parseInt(timeInput);
+            if (isNaN(time) || time <= 0) {
+                alert("Veuillez entrer un nombre valide pour le temps.");
+                return;
+            }
+            const data = {
+                mmsi: response[selectedIndex].mmsi,
+                Length: response[selectedIndex].longueur,
+                Width: response[selectedIndex].largeur,
+                Draft: response[selectedIndex].draft,
+                Latitude: response[selectedIndex].lat,
+                Longitude: response[selectedIndex].lon,
+                sog: response[selectedIndex].sog,
+                cog: response[selectedIndex].cog,
+                heading: response[selectedIndex].heading
+            };
+            const queryString = new URLSearchParams(data).toString();
+            ajaxRequest('POST', `../php/request.php?action=PredictPosition&time=${time}`, displayPredictionPosition, queryString);
+        } else {
+            alert("Veuillez sélectionner un navire pour la prédiction.");
+        }
+    });
+}
 
-    if (selectedIndex !== undefined) {
-      const data = {
-      mmsi: response[selectedIndex].mmsi,
-      Length: response[selectedIndex].longueur,
-      Width: response[selectedIndex].largeur,
-      Draft: response[selectedIndex].draft
+function plotAllBoatsOnMap(response) {
+    if (response.length === 0) return;
+
+let data = [{
+    type: "scattermapbox",
+    lat: response.map(boat => boat.lat),
+    lon: response.map(boat => boat.lon),
+    text: response.map(boat => 
+        `${boat.nom_bateau}<br>SOG: ${boat.sog} kn<br>COG: ${boat.cog}°`
+    ),
+    marker: { 
+        color: response.map(() => getRandomColor()),
+        size: 8
+    },
+    hoverinfo: 'text',
+    mode: 'markers'
+}];
+
+
+    let layout = {
+        dragmode: "zoom",
+        mapbox: {
+            style: "open-street-map",
+            center: {
+                lat: 25,
+                lon: -90
+            },
+            zoom: 4.25
+        },
+        margin: { r: 0, t: 0, b: 0, l: 0 },
+        showlegend: false
     };
-    console.log(data)
-    const queryString = new URLSearchParams(data).toString();
-    console.log(queryString)
 
+    Plotly.newPlot("mapDiv", data, layout);
+}
 
-      ajaxRequest('POST', '../php/request.php?action=predictType',displayPredictPage,queryString);
-
-    //console.log(`Prédiction du type pour le navire MMSI: ${response[selectedIndex].mmsi}`);
-    } else {
-      alert("Veuillez sélectionner un navire pour la prédiction.");
-    }
-  })
-
-  $('#positionButton').click(() => {
-    let selectedIndex = $("input[name='Prediction-Type-Navire']:checked").data('index');
-    if (selectedIndex !== undefined) {
-
-      let timeInput = prompt("Entrez le temps (en secondes) pour la prédiction de position :");
-      let time = parseInt(timeInput);
-      if (isNaN(time) || time <= 0) {
-        alert("Veuillez entrer un nombre valide pour le temps.");
-        return;
-      }
-      const data = {
-      mmsi: response[selectedIndex].mmsi,
-      Length: response[selectedIndex].longueur,
-      Width: response[selectedIndex].largeur,
-      Draft: response[selectedIndex].draft,
-      Latitude: response[selectedIndex].lat,
-      Longitude: response[selectedIndex].lon,
-      sog: response[selectedIndex].sog,
-      cog: response[selectedIndex].cog,
-      heading: response[selectedIndex].heading,
-
-    };
-      const queryString = new URLSearchParams(data).toString();
-      console.log(queryString)
+function plotSelectedBoatOnMap(response, index) {
+    let selectedRow = response[index];
     
-      ajaxRequest('POST', `../php/request.php?action=PredictPosition`, displayPredictionPosition, queryString);
-    } else {
-      alert("Veuillez sélectionner un navire pour la prédiction.");
+    let data = [{
+        type: "scattermapbox",
+        lat: [selectedRow.lat],
+        lon: [selectedRow.lon],
+        text: [selectedRow.nom_bateau],
+        marker: { 
+            color: "red",
+            size: 3 
+        },
+        hoverinfo: 'text',
+        mode: 'markers'
+    }];
+
+    let layout = {
+        dragmode: "zoom",
+        mapbox: {
+            style: "open-street-map",
+            center: {
+                lat: selectedRow.lat,
+                lon: selectedRow.lon
+            },
+            zoom: 3
+        },
+        margin: { r: 0, t: 0, b: 0, l: 0 }
+    };
+
+    Plotly.newPlot("mapDiv", data, layout);
+}
+
+// Fonction utilitaire pour générer des couleurs aléatoires
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
-  })
-  }
+    return color;
+}
